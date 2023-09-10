5a22851c42f033e9b265840e51980bd4503d0bc7
VictorChukwudi <victorukay@gmail.com>
2023-09-10 21:09:39 +0100

    final

--- server.js
+++ server.js
@@ -32,19 +32,26 @@ const timeFunc = (number) => {
 fastify.get("/api", async (req, reply) => {
   try {
     const { slack_name, track } = req.query;
-    const d = new Date();
-    const github_file_url =
-      "https://github.com/VictorChukwudi/hng_stage_one/blob/main/server.js";
-    const github_repo_url = "https://github.com/VictorChukwudi/hng_stage_one";
-    reply.code(200).send({
-      slack_name,
-      current_day: timeFunc(d.getDay()),
-      utc_time: d.toISOString(),
-      track,
-      github_file_url,
-      github_repo_url,
-      status_code: 200,
-    });
+    if (!slack_name || !track) {
+      reply.code(400).send({
+        status: "error",
+        msg: "slack_name and track are required as query paramters",
+      });
+    } else {
+      const d = new Date();
+      const github_file_url =
+        "https://github.com/VictorChukwudi/hng_stage_one/blob/main/server.js";
+      const github_repo_url = "https://github.com/VictorChukwudi/hng_stage_one";
+      reply.code(200).send({
+        slack_name,
+        current_day: timeFunc(d.getDay()),
+        utc_time: d.toISOString(),
+        track,
+        github_file_url,
+        github_repo_url,
+        status_code: 200,
+      });
+    }
   } catch (error) {
     console.log(error);
   }
